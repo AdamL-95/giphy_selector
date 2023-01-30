24f386802a0bcf506553e148aef0e376068e0a87
Adam Lynch <adamlynch95@gmail.com>
2023-01-29 23:19:39 -0600

    Enforce types

--- src/lib/getRandomData.ts
+++ src/lib/getRandomData.ts
@@ -1,10 +1,12 @@
-const getRandomData = async () => {
+import { SingleResponse } from "giphy-api"
+
+const getRandomData = async (): Promise<SingleResponse> => {
   const api_key = "5FoJPpL8icr9B00Dig8eRZmlE0rjPfHf"
   const result = await fetch(
     `https://api.giphy.com/v1/gifs/random?api_key=${api_key}&rating=g`
   )
   const data = await result.json()
-  return data
+  return data as Promise<SingleResponse>
 }
 
 export default getRandomData
--- src/lib/getSearchData.ts
+++ src/lib/getSearchData.ts
@@ -1,7 +1,9 @@
+import { MultiResponse } from "giphy-api"
+
 const getSearchData = async (
   searchQuery: string | string[],
   offset: string | string[]
-) => {
+): Promise<MultiResponse> => {
   const api_key = "5FoJPpL8icr9B00Dig8eRZmlE0rjPfHf"
   const result = await fetch(
     `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${searchQuery}&rating=g&limit=24&offset=${offset}`
